cbc1b39796a22fd03efe913d52a17f6f1a310cc0
Hung Nguyen <hungdhnt@gmail.com>
2021-10-31 22:07:07 +0700

    update

--- topics/04-classes-and-objects/src/PhanSo/Program.cs
+++ topics/04-classes-and-objects/src/PhanSo/Program.cs
@@ -1,17 +1,5 @@
 ﻿/*
-Xử lý phân số
-
-Mục tiêu: 
-- Giải quyết một bài toán đơn giản theo phương pháp hướng đối tượng
-- Biết xây dựng lớp, tạo đối tượng, sử dụng đối tượng
-- Biết thiết lập bao đóng (encapsulation) để che dấu dữ liệu:
-    * Phân biệt được trường dữ liệu (fields) và thuộc tính (properties)
-    * Đặt mức truy cập private cho các trường dữ liệu
-    * Khi muốn cho phép truy cập đến các trường dữ liệu này từ bên ngoài lớp, tạo thuộc tính dùng chung (public)
-      với phương thức get (cho phép đọc), set (cho phép thay đổi)
-- Dùng quy cách đặt tên (coding conventions) thống nhất (theo đề xuất của Microsoft)
-  https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-
+Chương trình xử lý lớp phân số
 */
 
 using System;
@@ -27,9 +15,9 @@ class Program
         p1.Xuat();   // Gọi phương thức Xuat()
         Console.WriteLine("Tu so = {0}", p1.TuSo);   // Thực hiện được vì thuộc tính TuSo cho phép đọc {get;} giá trị _tuSo
         Console.WriteLine("Mau so = {0}", p1.MauSo); // Thực hiện được vì thuộc tính MauSo cho phép đọc {get;} giá trị _mauSo       
-        //p1.TuSo = 10;                                // Thực hiện được vì thuộc tính TuSo cho phép thay đổi {set;} giá trị _tuSo
+        //p1.TuSo = 10;                              // Thực hiện được vì thuộc tính TuSo cho phép thay đổi {set;} giá trị _tuSo
         Console.WriteLine("Tu so = {0}", p1.TuSo);       
-        // p.MauSo = 10;                            // Không thực hiện được vì thuộc tính MauSo không cho thay đổi {set;} giá trị _mauSo
+        // p.MauSo = 10;                             // Không thực hiện được vì thuộc tính MauSo không cho thay đổi {set;} giá trị _mauSo
         // Tối giản phân số
         p1.ToiGian();
         // In ra phân số tối giản:
@@ -47,6 +35,44 @@ class Program
         p1.ToiGian();
         p1.Xuat();
 
+        // Tạo một mảng phân số
+        Console.Write("Nhap so luong phan so: ");
+        int numberOfElements = int.Parse(Console.ReadLine());
+        PhanSo[] psList = new PhanSo[numberOfElements];
+        
+        // Nhập mảng phân số từ bàn phím
+        for(int i=0; i < numberOfElements; i++)
+        {
+          psList[i] = new PhanSo();
+          psList[i].Nhap();
+        }
+
+        // In ra mảng phân số
+        Console.WriteLine("Cac phan so vua nhap: ");
+        for(int i=0; i < numberOfElements; i++)
+          psList[i].Xuat();
+
+        // Tìm phân số lớn nhất
+        PhanSo psMax = new PhanSo(psList[0]); // psMax = psList[0]  (gọi hàm thiết lập sao chép)
+        for(int i=1; i < numberOfElements; i++)
+          if(psMax.GiaTri() < psList[i].GiaTri()) psMax = psList[i];
+        Console.WriteLine("Phan so lon nhat la: ");
+        psMax.Xuat();
+
+        // Sắp xếp danh sách phân số tăng dần
+        for(int i=0; i < numberOfElements - 1; i++)
+          for(int j=i+1; j < numberOfElements; j++)
+            if(psList[i].GiaTri() > psList[j].GiaTri())
+            {
+              PhanSo tmp = new PhanSo(psList[i]);
+              psList[i] = psList[j];
+              psList[j] = tmp;
+            }
+
+        // In ra mảng phân số đã sắp xếp
+        Console.WriteLine("Cac phan so theo thu tu tang dan: ");
+        for(int i=0; i < numberOfElements; i++)
+          psList[i].Xuat();
     }
 }
 
