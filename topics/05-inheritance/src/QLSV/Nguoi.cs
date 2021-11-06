60ba9a26418811f33eceb88489a1b55cd0af97ad
Hung Nguyen <hungdhnt@gmail.com>
2021-11-06 19:45:50 +0700

    add example to topic 5

---
+++ topics/05-inheritance/src/QLSV/Nguoi.cs
@@ -0,0 +1,28 @@
+/*
+Khai báo lớp người
+*/
+
+using System;
+
+class Nguoi
+{
+    protected string _hoTen;
+    bool _gioiTinh;
+    string _ngaySinh;
+    
+    // Hàm nhập
+    public void Nhap()
+    {
+        Console.Write("Ho ten: ");
+        _hoTen = Console.ReadLine();
+        Console.Write("Gioi tinh : ");
+        _gioiTinh = bool.Parse(Console.ReadLine());
+    }
+
+    // Hàm xuất
+    public void Xuat()
+    {
+        Console.WriteLine("{0} - {1}", _hoTen, _gioiTinh);
+    }
+
+}
---
+++ topics/05-inheritance/src/QLSV/SinhVien.cs
@@ -0,0 +1,30 @@
+/*
+Cài đặt lớp Sinh viên kế thừa lớp Người
+*/
+
+using System;
+class SinhVien: Nguoi
+{
+    string _ma;
+    double _dtb;
+    
+    // Phương thức nhập
+    public new void Nhap()
+    {
+        base.Nhap(); // Gọi phương thức nhập của lớp cha
+        // Nhập thêm dữ liệu cho lớp con
+        Console.Write("Nhap MSSV: ");
+        _ma = Console.ReadLine();
+        Console.Write("Nhap DTB: ");
+        _dtb = double.Parse(Console.ReadLine()); 
+    }
+
+    // Phương thức xuất
+    public new void Xuat()
+    {
+        base.Xuat(); // Gọi phương thức xuat của lớp cha
+        // In thông tin của lớp con
+        Console.WriteLine("MSSV: " + _ma);
+        Console.Write("DTB: {0}", _dtb);
+     }
+}
