65284a5038d13b454a96415f318ac275fe0078f8
Hung Nguyen <hungdhnt@gmail.com>
2021-11-19 19:23:35 +0700

    update

--- topics/05-inheritance/examples/01-inheritance/QuanlyXe/Xe.cs
+++ topics/05-inheritance/examples/01-inheritance/QuanlyXe/Xe.cs
@@ -1,18 +1,3 @@
-/*
-Xây dựng lớp Xe gồm các thuộc tính: Biển số (vd: 79A-12345), năm sản xuất (vd: 2019), giá (đơn vị tính: triệu đồng).
-Xây dựng lớp XeCon kế thừa lớp Xe và có thêm thuộc tính: số chỗ ngồi, loại xe (sedal/SUV/bán tải).
-Yêu cầu:
-a) Cài đặt các lớp với các phương thức sau:
-- Phương thức thiết lập có tham số và không có tham số.
-- Phương thức Nhap() để nhập thông tin xe.
-- Phương thức Xuat() để in thông tin xe.
-b) Nhập danh sách n xe con (0 < n < 50)
-- In ra danh sách xe và thông số kèm theo.
-- Tìm xe có giá thấp nhất, cao nhất.
-- Nhập 2 chữ số đầu biển số (ví dụ 79), in ra tất cả xe thuộc tỉnh có biển số đó.
-- Sắp xếp danh sách xe theo thứ tự tăng dần của năm sản xuất, in ra danh sách sau sắp xếp.
-*/
-
 using System;
 
 // Cài đặt lớp xe
