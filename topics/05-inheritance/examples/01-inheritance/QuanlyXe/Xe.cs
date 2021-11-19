e68d4cc2b6151ec3f373c77071b44460de8fd2b1
Hung Nguyen <hungdhnt@gmail.com>
2021-11-19 19:15:01 +0700

    update

--- topics/05-inheritance/examples/01-inheritance/QuanlyXe/Xe.cs
+++ topics/05-inheritance/examples/01-inheritance/QuanlyXe/Xe.cs
@@ -6,7 +6,8 @@ a) Cài đặt các lớp với các phương thức sau:
 - Phương thức thiết lập có tham số và không có tham số.
 - Phương thức Nhap() để nhập thông tin xe.
 - Phương thức Xuat() để in thông tin xe.
-b) Nhập danh sách n xe con (0 - In ra danh sách xe và thông số kèm theo.
+b) Nhập danh sách n xe con (0 < n < 50)
+- In ra danh sách xe và thông số kèm theo.
 - Tìm xe có giá thấp nhất, cao nhất.
 - Nhập 2 chữ số đầu biển số (ví dụ 79), in ra tất cả xe thuộc tỉnh có biển số đó.
 - Sắp xếp danh sách xe theo thứ tự tăng dần của năm sản xuất, in ra danh sách sau sắp xếp.
